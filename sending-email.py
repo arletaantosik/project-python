3030ec1e81786571abbb6e31dadd00a8bb497377
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-05-12 23:20:09 +0200

    Update sending-email.py

--- sending-email.py
+++ sending-email.py
@@ -1,5 +1,6 @@
 import yagmail #to send emails by gmail in Python
 import os #environmental variables
+import time
 
 sender = '@gmail.com'
 receiver = '@gmail.com'
@@ -12,6 +13,8 @@ this is the content of the email
 content2
 """
 
-yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD')) #creates SMTP object, Simple Mail Transfer Protocol, only to send email
-yag.send(to=receiver, subject=subject, contents=contents)
-print("email sent")
+while True:
+  yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD')) #creates SMTP object, Simple Mail Transfer Protocol, only to send email
+  yag.send(to=receiver, subject=subject, contents=contents)
+  print("email sent")
+  time.sleep(60) # so the same email is being sent every 60 seconds
